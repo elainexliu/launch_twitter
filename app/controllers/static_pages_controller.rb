a1ec058e535bf0d24cfcc2535ad9b3ffa4acdfaf
Elaine <eliu0499@gmail.com>
2020-07-11 18:42:17 -0400

    updated the writing on the home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
     def main_page
-    	@mission_statement = "save the world"
+    	@mission_statement = "Our mission is to save the world."
+    	@intro = "Welcome to the new and improved Twitter! It's much better than the other version:) Enjoy your time here!"
     end
 
 end
