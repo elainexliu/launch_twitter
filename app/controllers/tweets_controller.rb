605821fe83871192835f07db431116ec3751e8b9
Elaine <eliu0499@gmail.com>
2020-07-12 14:50:30 -0400

    must login before tweeting

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,15 @@
 class TweetsController < ApplicationController
+	
+	before_action :authenticate_user!
+
 	def new
 		@tweet = Tweet.new
 
 	end
 	def create
+		
 		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
 		if @tweet.save
 			flash[:success] = "Your tweet was successfully published!"
 			redirect_to new_tweet_path
