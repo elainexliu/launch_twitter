cc9f71af4fca3585598df8081d9ac5a08bc2c2c5
Elaine <eliu0499@gmail.com>
2020-07-12 11:48:03 -0400

    made the success notification alert

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,12 @@
 class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
+
 	end
 	def create
 		@tweet = Tweet.new(tweet_params)
 		@tweet.save
+		flash[:success] = "Your tweet was successfully published!"
 		redirect_to new_tweet_path
 	end
 
