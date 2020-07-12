c68255adcf2c07e05c1452e526507eff148a4a7c
Elaine <eliu0499@gmail.com>
2020-07-12 16:30:28 -0400

    made profile signup form upon creating an account

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,21 @@ class ProfilesController < ApplicationController
 	before_action :authenticate_user!
 	def new
 		@profile = Profile.new
-		
+	end
+
+	def create
+		@profile = Profile.new(profile_params)
+		@profile.user = current_user
+		if @profile.save
+			flash[:success] = "Your profile was successfully updated!"
+			redirect_to new_profile_path
+		else
+			render 'new'
+		end
+	end
+
+	def profile_params
+		params.require(:profile).permit(:first_name, :last_name, :age)
 	end
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
     def main_page
-    	@mission_statement = "Our mission is to save the world."
-    	@intro = "Welcome to the new and improved Twitter! It's much better than the other version:) Enjoy your time here!"
-    end
+    	if user_signed_in?
+    		if current_user.profile
+    			redirect_to new_tweet_path
+    		else
+    			redirect_to new_profile_path
+    		end
+    	else
+	    	@mission_statement = "Our mission is to save the world."
+	    	@intro = "Welcome to the new and improved Twitter! It's much better than the other version:) Enjoy your time here!"
+        end
+     end
 
 end
